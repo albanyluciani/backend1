0a6af4adf22225fe0d2f7087cccc1bb376b6e798
albanyluciani <lucianialbany@gmail.com>
2025-07-25 20:37:21 -0500

    Avance proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+
+import tasks.models.Task;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Task task = new Task("Encender", "Encender el TV", false);
+        System.out.println("Tarea Pendiente:" + task.getName() + "Descripcion:" + task.getDescription() + task.getStatus());
+        task.setStatus(true); 
+        System.out.println("Tarea Finalizada" + task.getName() + "Descipcion:" + task.getDescription());
     }
 }
--- src/tasks/models/Task.java
+++ src/tasks/models/Task.java
@@ -10,4 +10,28 @@ public class Task {
         this.description = description;
         this.status = status;
     }
+
+    // Setters
+    public void setName(String name){
+        this.name = name;
+            }
+
+    public void setDescription(String description){
+        this.description = description;
+            }
+    public void setStatus(boolean status){
+        this.status = status;
+            }
+
+    // Getters
+    public String getName(){
+        return this.name;
+            }
+
+    public String getDescription(){
+        return this.description;
+            }
+    public boolean getStatus(){
+        return this.status;
+
 }
