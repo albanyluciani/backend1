c5278ea991c3392fae1fbb5c751c554bcbc39fca
albanyluciani <lucianialbany@gmail.com>
2025-08-01 21:01:17 -0500

    ejercicio

--- src/App.java
+++ src/App.java
@@ -5,7 +5,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Task task = new Task("Encender", "Encender el TV", false);
-        System.out.println("Tarea Pendiente:" + task.getName() + "Descripcion:" + task.getDescription() + task.getStatus());
+        System.out.println("Tarea Pendiente:" + 
+        task.getName() + 
+        "Descripcion:" + 
+        task.getDescription() + 
+        task.getStatus());
         task.setStatus(true); 
         System.out.println("Tarea Finalizada"+ task.getName() + "Descripcion:" + task.getDescription() + task.getStatus());
     }
--- src/tasks/models/useCase/TaskUseCase.java
+++ src/tasks/models/useCase/TaskUseCase.java
@@ -2,6 +2,9 @@ package tasks.models.useCase;
 
 import tasks.models.Task;
 
+//Instancia es un hilo
+
+// Contenedor de metodos y atributos
 public class TaskUseCase {
 
     //Propiedad
@@ -9,13 +12,17 @@ public class TaskUseCase {
 
     //Contructor
     public TaskUseCase (){
-        this.task = new Task();
+        this.task = new Task(); //Forma #1 de crear una tarea sin valores
     }
 
      public String create(String name, String description){
-     Task task = new Task(name, description, false);
+     Task task = new Task(name, description, false); //Forma #2 de crear una tarea con valores
      return "Tarea Creada:" + task.getName() + "Descripcion" + task.getDescription();
     }
+
+    //Update
+    public
+    task.setName
 
     //Get
     public String get (){
